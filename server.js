c8ca2b319b6a281f41acd167f16771251648e946
Amine Aouragh <71039249+AmineAouragh@users.noreply.github.com>
2021-07-18 14:24:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const { v4: uuidv4 } = require("uuid")
 
 const app = express()
 
@@ -7,10 +8,32 @@ const server = require("http").Server(app)
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
+const io = require("socket.io")(server)
+const { ExpressPeerServer } = require("peer")
+
+const peerServer = ExpressPeerServer(server, { debug: true })
+
+app.use("/peerjs", peerServer)
+
 
 app.get("/", (req, res) => {
 
+    res.redirect(`/${uuidv4()}`)  // create a random unique id for each room and redirecting the user to that room
+
     res.render('room')
+})
+
+app.get("/:room", (req, res) => {
+
+    res.render("room", { roomId: req.param.room })
+
+})
+
+io.on("connection", socket => {
+  socket.on("join-room", (roomId, userId) => {
+     socket.join(roomId)
+     socket.to(roomId).broadcast.emit("user-connected", userId)
+  })
 })
 
 
